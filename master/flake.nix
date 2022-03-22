ce0d21f10c0214810dd06d30f11f7fec53addc39
hugosenari <hugosenari@gmail.com>
2022-03-22 00:36:25 -0300

    chore: re-index ../nimpkgs/p/pcap

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Tiny pure Nim library to read PCAP files used by TcpDump/WinDump/Wireshark.'';
+  description = ''Tiny pure Nim library to read PCAP files'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-pcap-master.flake = false;
   inputs.src-pcap-master.owner = "PMunch";
-  inputs.src-pcap-master.ref   = "refs/heads/master";
+  inputs.src-pcap-master.ref   = "master";
   inputs.src-pcap-master.repo  = "nim-pcap";
   inputs.src-pcap-master.type  = "github";
   
